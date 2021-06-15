36c493757bd84542d90f360b413b7d6ae479868c
rtldg <55846624+rtldg@users.noreply.github.com>
2021-06-15 22:31:54 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -ev
-read -r GH_ACCESS_TOKEN <<< $(bw list items --search 'github - PMArkive
-' | jq --raw-output '.[].fields[] | select(.name=="access token").value');
+read -r GH_ACCESS_TOKEN <<< $(bw list items --search 'github - PMArkive' | jq --raw-output '.[].fields[] | select(.name=="access token").value');
 export GH_ACCESS_TOKEN
 
 python3 forksyncer.py
